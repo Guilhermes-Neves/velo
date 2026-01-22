a36414161034dcff740707f1c62cd7a91529b3c2
Guilherme Neves <guilhermes_neves@hotmail.com>
2026-01-22 12:00:26 -0300

    iniciando o teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  
+  // Checkpoint 1: Verificar se a página inicial esta carregada
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  // Checkpoint 2: Verificar se a página de consulta de pedido esta carregada
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await page.getByTestId('search-order-id').fill('VLO-FPWLMY');
+  await page.getByTestId('search-order-button').click();
+  
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-FPWLMY');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
