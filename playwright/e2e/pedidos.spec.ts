ade7a19f6a9f1e26e00aa5f3781db4c5ab132dc2
Guilherme Neves <guilhermes_neves@hotmail.com>
2026-01-23 19:28:12 -0300

    primeiro desafio do curso

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/');
-  
-  // Checkpoint 1: Verificar se a página inicial esta carregada
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint 2: Verificar se a página de consulta de pedido esta carregada
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-  await page.getByTestId('search-order-id').fill('VLO-FPWLMY');
-  await page.getByTestId('search-order-button').click();
+
+  // Act
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-FPWLMY');
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
   
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-FPWLMY');
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  // Assert
+  await expect(page.getByText('VLO-FPWLMY')).toBeVisible({timeout: 10_000});
+  await expect(page.getByText('VLO-FPWLMY')).toContainText('VLO-FPWLMY');
+  
+  await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible();
+  // Como deixei a estratégia de buscar pelo texto, comentei o assert pelo texto, pois seria redundante. Com o assert acima caso o texto esteja incorreto o teste falhará.
+  //await expect(page.locator('//div[text()="APROVADO"]')).toContainText('APROVADO');
 });
