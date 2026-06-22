c6119bca370ff4a1d03c8070895508fc68152b31
Guilherme Neves <guilhermes_neves@hotmail.com>
2026-06-22 10:20:50 -0300

    versão final do Page object

--- playwright/support/pages/OrderLookupPage.ts
+++ playwright/support/pages/OrderLookupPage.ts
@@ -3,25 +3,37 @@ import { Page, expect } from '@playwright/test'
 type OrderStatus = 'APROVADO' | 'REPROVADO' | 'EM_ANALISE'
 
 export class OrderLockupPage {
-    private readonly statusClasses = {
-        APROVADO: ['bg-green-100', 'text-green-700', 'lucide-circle-check-big'],
-        REPROVADO: ['bg-red-100', 'text-red-700', 'lucide-circle-x'],
-        EM_ANALISE: ['bg-amber-100', 'text-amber-700', 'lucide-clock']
+  constructor(private page: Page) { }
+
+  async searchOrder(code: string) {
+    await this.page.getByRole('textbox', { name: 'Número do Pedido' }).fill(code)
+    await this.page.getByRole('button', { name: 'Buscar Pedido' }).click()
+  }
+
+  async validateStatusBadge(status: OrderStatus) {
+    const statusClasses = {
+      APROVADO: {
+        backgroudColor: 'bg-green-100',
+        text: 'text-green-700',
+        icon: 'lucide-circle-check-big'
+      },
+      REPROVADO: {
+        backgroudColor: 'bg-red-100',
+        text: 'text-red-700',
+        icon: 'lucide-circle-x'
+      },
+      EM_ANALISE: {
+        backgroudColor: 'bg-amber-100',
+        text: 'text-amber-700',
+        icon: 'lucide-clock'
+      }
     } as const
 
-    constructor(private page: Page) { }
+    const classes = statusClasses[status]
+    const statusBadge = this.page.getByRole('status').filter({ hasText: status })
 
-    async searchOrder(code: string) {
-        await this.page.getByRole('textbox', { name: 'Número do Pedido' }).fill(code)
-        await this.page.getByRole('button', { name: 'Buscar Pedido' }).click()
-    }
-
-    async validateStatusBadge(status: OrderStatus) {
-        const [bgClass, textClass, iconClass] = this.statusClasses[status]
-        const statusBadge = this.page.getByRole('status').filter({ hasText: status })
-
-        await expect(statusBadge).toHaveClass(new RegExp(bgClass))
-        await expect(statusBadge).toHaveClass(new RegExp(textClass))
-        await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(iconClass))
-    }
+    await expect(statusBadge).toHaveClass(new RegExp(classes.backgroudColor))
+    await expect(statusBadge).toHaveClass(new RegExp(classes.text))
+    await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(classes.icon))
+  }
 }
